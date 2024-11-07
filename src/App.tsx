1d30066e6c0860dfaf77757380032d89a96ce673
rzzza_rehan <rhn.raza121@gmail.com>
2024-11-07 22:46:16 +0530

    login ui done here

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { Container, createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+import Auth from './components/auth/Auth';
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark'
+  }
+});
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Container>
+        <Auth />
+      </Container>
+    </ThemeProvider>
+  )
 }
 
 export default App;
---
+++ src/components/auth/Auth.tsx
@@ -0,0 +1,33 @@
+import { Stack, Button, TextField } from '@mui/material';
+import { useState } from 'react';
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return <>
+    <Stack spacing={3} sx={{
+      height: "100vh",
+      margin: "0 auto",
+      justifyContent: "center",
+      maxWidth: {
+        xs: "70%",
+        md: "30%"
+      }
+    }}>
+      <TextField
+        type="email"
+        label="Email"
+        variant='outlined'
+        value={email}
+        onChange={(event) => setEmail(event?.target.value)} />
+      <TextField
+        type="password"
+        label="Password"
+        variant='outlined'
+        value={ password }
+        onChange={(event) => setPassword(event?.target.value)} />
+      <Button variant='contained'>Login</Button>
+    </Stack>
+  </>
+}
+export default Auth;

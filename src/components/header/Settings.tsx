fc0527309e57249ad96816e1010a8e2cb8627929
rzzza_rehan <rhn.raza121@gmail.com>
2024-12-17 23:31:32 +0530

    logout ui added

--- src/components/header/Settings.tsx
+++ src/components/header/Settings.tsx
@@ -6,12 +6,12 @@ import Avatar from '@mui/material/Avatar';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import { useState } from 'react';
-
-const settings = ['Logout'];
+import { useLogout } from '../../hooks/useLogout';
+import { onLogout } from '../../utils/logout';
 
 const Settings = () => {
   const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
-
+  const { logout } = useLogout();
   const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElUser(event.currentTarget);
   };
@@ -42,11 +42,15 @@ const Settings = () => {
         open={Boolean(anchorElUser)}
         onClose={handleCloseUserMenu}
       >
-        {settings.map((setting) => (
-          <MenuItem key={setting} onClick={handleCloseUserMenu}>
-            <Typography sx={{ textAlign: 'center' }}>{setting}</Typography>
-          </MenuItem>
-        ))}
+        <MenuItem 
+          key='logout' 
+          onClick={async () => {
+            await logout();
+            onLogout();
+            handleCloseUserMenu();
+          }}>
+          <Typography sx={{ textAlign: 'center' }}>Logout</Typography>
+        </MenuItem>
       </Menu>
     </Box>
   </>

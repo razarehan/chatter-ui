d0225a9ebabb11b6f972be0a91fbd7f934b7ffe9
rzzza_rehan <rhn.raza121@gmail.com>
2024-12-21 20:10:54 +0530

    create "Add Chat Modal"

---
+++ src/components/chat-list/chat-list-add/ChatListAdd.tsx
@@ -0,0 +1,62 @@
+import { Box, Button, FormControlLabel, FormGroup, IconButton, InputBase, Modal, Paper, Stack, Switch, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+import SearchIcon from "@mui/icons-material/Search";
+
+interface ChatListAddProps {
+  open: boolean;
+  handleClose: () => void;
+}
+const ChatListAdd = ({ open, handleClose }: ChatListAddProps) => {
+  const [isPrivate, setIsPrivate] = useState(true);
+  return <>
+    <Modal
+      open={open}
+      onClose={handleClose}
+    >
+      <Box
+        sx={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: 400,
+          bgcolor: "background.paper",
+          border: "2px solid #000",
+          boxShadow: 24,
+          p: 4,
+        }}
+      >
+        <Stack spacing={2}>
+          <Typography variant="h6" component="h2">Add Chat</Typography>
+          <FormGroup>
+            <FormControlLabel
+              style={{ width: 0 }}
+              control={
+                <Switch
+                  defaultChecked
+                  value={isPrivate}
+                  onChange={(event) => setIsPrivate(event.target.checked)} />
+              }
+              label="private"
+            />
+          </FormGroup>
+          {
+            isPrivate ? (
+              <Paper sx={{ p: "2px 4px", display: "flex", alignItems: "center" }}>
+                <InputBase sx={{ ml: 1, flex: 1 }} placeholder="Search Users" />
+                <IconButton sx={{ p: "10px" }}>
+                  <SearchIcon />
+                </IconButton>
+              </Paper>
+            ) : (
+              <TextField label="Name" />
+            )
+          }
+          <Button variant="outlined">Save</Button>
+        </Stack>
+      </Box>
+    </Modal>
+  </>
+}
+
+export default ChatListAdd;
--- src/components/chat-list/chat-list-header/ChatListHeader.tsx
+++ src/components/chat-list/chat-list-header/ChatListHeader.tsx
@@ -1,12 +1,15 @@
 import { AddCircle } from "@mui/icons-material";
 import { AppBar, Toolbar, IconButton } from "@mui/material";
 
-const ChatListHeader = () => {
+interface ChatListHeaderProps {
+  handleAddChat: () => void;
+}
+const ChatListHeader = ({ handleAddChat }: ChatListHeaderProps) => {
   return (
     <AppBar position="static" color="transparent">
       <Toolbar >
-        <IconButton size="large" edge="start">
-          <AddCircle/>
+        <IconButton size="large" edge="start" onClick={handleAddChat}>
+          <AddCircle />
         </IconButton>
       </Toolbar>
     </AppBar>
